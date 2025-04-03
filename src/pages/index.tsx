88abaadf80fe208844462a260fd032ce8890489e
baejb <bjb6478@naver.com>
2025-04-03 23:41:07 +0900

    스타일 적용하기

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,10 @@
+//CSS Modules page 별로 className 겹치는거 방지하기 위해 app 컴포넌트 외에는 글로벌 css 파일 임포트 불가 대신 module 사용
+import style from "./index.module.css";
 export default function Home() {
-  return <h1>인덱스</h1>;
+  return (
+    <>
+      <h1 className={style.h1}>인덱스</h1>
+      <h2 className={style.h2}> h2</h2>
+    </>
+  );
 }
