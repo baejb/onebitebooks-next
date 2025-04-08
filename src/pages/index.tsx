a09f0e92ba80dccec6c1c00ef189b85af751360b
baejb <bjb6478@naver.com>
2025-04-08 19:22:12 +0900

    SSR 1 실습 : getServerSiceProps에 대해

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,29 @@
 //CSS Modules page 별로 className 겹치는거 방지하기 위해 app 컴포넌트 외에는 글로벌 css 파일 임포트 불가 대신 module 사용
 import SearchableLayout from "@/components/searchable-layout";
 import style from "./index.module.css";
-import { ReactNode } from "react";
+import { ReactNode, useEffect } from "react";
 import books from "@/mock/books.json"; //@ 프로젝트 src 가리키는 경로
 import BookItem from "@/components/book-item";
-export default function Home() {
+import { InferGetServerSidePropsType } from "next";
+
+export const getServerSideProps = () => {
+  // 컴포넌트보다 먼저 실행되어 , 컴포넌트에 필요한 데이터 불러오는 함수
+  const data = "hello";
+  return {
+    props: {
+      data,
+    },
+  };
+};
+
+export default function Home({
+  data,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) {
+  console.log(data);
+
+  useEffect(() => {
+    console.log(window);
+  }, []);
   return (
     <div className={style.container}>
       <section>
