73b93ad604b7d480e0b052e799aa74a01c12b178
baejb <bjb6478@naver.com>
2025-04-21 14:58:43 +0900

    SSG 폴백옵션 설정하기

--- src/pages/book/[id].tsx
+++ src/pages/book/[id].tsx
@@ -7,6 +7,8 @@ import {
   InferGetStaticPropsType,
 } from "next";
 import fetchOneBook from "@/lib/fetch-one-book";
+import { useRouter } from "next/router";
+import { notFound } from "next/navigation";
 
 export const getStaticPaths = () => {
   return {
@@ -15,13 +17,21 @@ export const getStaticPaths = () => {
       { params: { id: "2" } },
       { params: { id: "3" } },
     ],
-    fallback: false,
+    fallback: true,
+    // false : 404 Not found,
+    // blocking : SSR 방식,
+    // ture: SSR 방식 + 데이터가 없는 폴백 상태의 페에지부터 반환 (그 뒤에 데이터는 후속으로)
   };
 };
 
 export const getStaticProps = async (context: GetStaticPropsContext) => {
   const id = context.params!.id;
   const book = await fetchOneBook(Number(id));
+  if (!book) {
+    return {
+      notFound: true,
+    };
+  }
   return {
     props: { book },
   };
@@ -30,6 +40,10 @@ export const getStaticProps = async (context: GetStaticPropsContext) => {
 export default function Page({
   book,
 }: InferGetStaticPropsType<typeof getStaticProps>) {
+  const router = useRouter();
+  if (router.isFallback) {
+    return <div>로딩중입니다.</div>;
+  }
   if (!book) return <div>문제 발생했습니다. 다시 시도해주세요</div>;
 
   const { id, title, subTitle, description, author, publisher, coverImgUrl } =
